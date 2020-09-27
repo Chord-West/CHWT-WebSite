1bb04b1dff50218da78a5da20202332e21376c53
KimHyunSeo <gustj2005@naver.com>
2020-09-27 17:32:40 +0900

    connected firebase database to boardpage

---
+++ js/board.js
@@ -0,0 +1,43 @@
+let menubtn = document.getElementsByClassName("menu_btn");
+
+
+
+
+menubtn[0].onclick = function(){
+    let navbar = document.getElementsByClassName("menu");
+    console.log(navbar);
+    if(navbar[0].style.display=='none'){
+        navbar[0].style.display='block';
+    }else{
+        navbar[0].style.display='none';
+    }
+}
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAbRit_OIY4dohuub5-CzMHaQfJu7G2-N4",
+    authDomain: "chord-west.firebaseapp.com",
+    databaseURL: "https://chord-west.firebaseio.com",
+    projectId: "chord-west",
+    storageBucket: "chord-west.appspot.com",
+    messagingSenderId: "940341606564",
+    appId: "1:940341606564:web:83b27a9bd69a114baa4e31",
+    measurementId: "G-9WMM1JDHV5"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+function writePost(username,title, body) {
+    // A post entry.
+    const postData = {
+      name: username,
+      title: title,
+      body: body,
+    };
+    // Get a key for a new Post.
+    const newPostKey = firebase.database().ref().child('posts').push(postData); 
+}
+
+console.log(database);
