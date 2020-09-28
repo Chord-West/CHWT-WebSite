bb93f4241d6df3e0ad0a4f1883d8deabd325b208
KimHyunSeo <gustj2005@naver.com>
2020-09-28 21:27:24 +0900

    login page update

--- js/board.js
+++ js/board.js
@@ -1,8 +1,7 @@
+let validcheck="false";
+
+
 let menubtn = document.getElementsByClassName("menu_btn");
-
-
-
-
 menubtn[0].onclick = function(){
     let navbar = document.getElementsByClassName("menu");
     console.log(navbar);
@@ -12,6 +11,7 @@ menubtn[0].onclick = function(){
         navbar[0].style.display='none';
     }
 }
+
 
 
 var firebaseConfig = {
@@ -39,5 +39,37 @@ function writePost(username,title, body) {
     // Get a key for a new Post.
     const newPostKey = firebase.database().ref().child('posts').push(postData); 
 }
+
+
+function getPosts(){
+    const postRef = firebase.database().ref('/posts/').once('value',function(snapshot){
+        const postData = Object.entries(snapshot.val());
+        for(let i=0; i<postData.length; i++){
+            const [key, body] = postData[i];
+            console.log(key,body);
+        }
+        // console.log(postData);
+    });
+
+    return ;
+}
+
+function validadmin(){
+    let loginform = document.loginForm;
+    let inputId = loginform.id.value;
+    let inputpassword = loginform.password.value;
+    let valid;
+    let valps;
+    console.log(adminId, password);
+    const validData = firebase.database().ref('/admin/').once('value',function(snapshot){
+        valid=snapshot.val().id;
+        valps=snapshot.val().password;
+        console.log(valid,valps);
+    });
+    if(adminId===valid&&password===valps){
+        console.log("success");
+    }
+}
+
 
 console.log(database);
