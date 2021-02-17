2fc19c0c2ecc38389507506603b85313388fb7ba
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2021-02-18 00:06:30 +0900

    Created the basic layout Using 'styled-components'

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import AppLayout from "./components/AppLayout";
+import {Switch,Route} from 'react-router-dom';
 
 const App = () => {
-    <div>App</div>;
-}
+    return(
+        <Switch>
+            <Route exact path="/" component={AppLayout}/>
+        </Switch>
+    );
+};
 
 export default App;
---
+++ front/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return(
+      <div style={{ backgroundColor : 'black', height : '50px', color :'#ffffff'}}>
+          Hello
+      </div>
+    );
+};
+
+export default Navbar;
